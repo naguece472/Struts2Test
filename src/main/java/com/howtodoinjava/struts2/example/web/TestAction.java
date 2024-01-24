4577f24b67533814ef92d84ddd9c5acd1147af71
Nagaraju Thiparthi <NThiparthi25@massmutual.com>
2024-01-24 16:43:18 +0530

    Struts2Test project added in personal github

---
+++ src/main/java/com/howtodoinjava/struts2/example/web/TestAction.java
@@ -0,0 +1,10 @@
+package com.howtodoinjava.struts2.example.web;
+
+
+public class TestAction
+{
+	public String execute(){
+        System.out.println("execute()....");
+        return "success";
+    }
+}
